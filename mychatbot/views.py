430ea11d68f8458200d687c2d6a8113a4333bf1e
yli431 <liuyahan0812@gmail.com>
2024-02-02 12:23:25 +1300

    dashboard elements updated v1.1

--- mychatbot/views.py
+++ mychatbot/views.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.http import JsonResponse
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
 from langchain_experimental.sql.base import SQLDatabaseChain
 
 def mychatbot(request: WSGIRequest) -> JsonResponse:
     question = request.GET["ai_question"]
-    answer = settings.DB_CHAIN_INSTANCE.invoke(input=question, return_only_outputs=False)
+    
+    # print("11111111111111111111")
+    # print(question)
+    
+    answer = settings.DB_CHAIN_INSTANCE.invoke(input=question, return_only_outputs=True)
     return JsonResponse({"ai_response": answer["result"]})
 
 def chat_history(request):
